ef8aeac69b47f70daf0f859678305120ea7de737
vlakondra <kondrashkin.wladimir@yandex.ru>
2021-12-27 06:17:27 +0000

    stop slots

--- src/Components/other.svelte
+++ src/Components/other.svelte
@@ -4,6 +4,7 @@
 
 <div style="width:30%;height:auto;background:yellow;margin:1px">
     {#each Object.entries({ ...data }) as [k, v]}
+
         <div>
             <div style="display:inline-block">{k}</div>
             <div style="display:inline-block">{v}</div>
--- src/Components/section_item.svelte
+++ src/Components/section_item.svelte
@@ -1,3 +1,27 @@
 <script>
-
+    // export let k;
+    // export let v;
 </script>
+
+<div>
+    <slot name="model">
+        <!-- <div>
+            <div style="display:inline-block">{k}</div>
+            <div style="display:inline-block">{v}</div>
+        </div> -->
+    </slot>
+    </div>
+    <div>
+    <slot name="address">
+        <span class="missing">Адрес не указан</span>
+    </slot>
+</div>
+
+
+
+<!-- <slot name='model'>
+    <div>
+        <div style="display:inline-block">{k}</div>
+        <div style="display:inline-block">{v}</div>
+    </div>
+    </slot> -->
