614f0c0d38e01e48d9c7e5e97ed54f19f4a74f07
vlakondra <kondrashkin.wladimir@yandex.ru>
2022-01-08 23:50:23 +0500

    did task for ekzam

---
+++ src/Components/app_content.svelte
@@ -0,0 +1,7 @@
+<script>
+    export let addr;
+</script>
+
+<div style="padding:3px">
+    <img src={addr} alt="app" />
+</div>
---
+++ src/Components/menu.svelte
@@ -0,0 +1,26 @@
+<script>
+    import { onMount } from "svelte";
+    export let data;
+    export let orient = "H";
+    export let topSelectFunc;
+
+    onMount(() => {
+        topSelectFunc(data[0].items);
+    });
+</script>
+
+<ul style="display: {orient == 'H' ? 'flex' : 'block'}">
+    {#each data as item}
+        <li on:click={() => topSelectFunc(item.items)}>{item.name}</li>
+    {/each}
+</ul>
+
+<style>
+    ul {
+        padding-left: 20px;
+        list-style: none;
+    }
+    ul li {
+        margin-right: 10px;
+    }
+</style>
